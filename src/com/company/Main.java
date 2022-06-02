f08aaa3b154186707d82d9027743880ae0bb07ac
Niexes <maria.kanavskaya@yandex.ru>
2022-06-02 20:56:03 +0300

    done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,14 +9,15 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         Stack<Double> numbers = new Stack<>();
 
-        String exp = sc.nextLine();
-        String expression2 = exp.replace(')', ' ');
+        String exp = sc.nextLine();//входящая строка
+        String m = convert(exp);//конвертированная строка в польскую запись
+        System.out.println(m); //проверка корректности конвертации
+
+        String expression2 = m.replace(')', ' ');
         String expression3 = expression2.replace('(', ' ');
 
 
         String[] newExp = expression3.split(" ");
-        // System.out.println(newExp.length);
-        // System.out.println(newe.length);
 
         for (int i = 0; i <= newExp.length - 1; i++) {
             if (check(newExp[i])) {
@@ -55,6 +56,75 @@ public class Main {
         System.out.println(result);
         if (!numbers.isEmpty()) {
             System.out.println("В стеке что-то осталось, некорректное выражение");
+        }
+    }
+
+    public static String convert(String exp) {
+        StringBuilder postfix = new StringBuilder();
+        Stack<Character> operators = new Stack<>(); //ТЕСТ 1 5*6+(2-9) //56*29-+
+//ТЕСТ 2 3+4*2/(1-5)*2
+//3 4 2 * 1 5 - / 2 * +
+//ТЕСТ 3 (8+2)*2-1*5/4+2 .. 8 2 + 2 * 1 5 * 4 / - 2 +
+        for (int i = 0; i < exp.length(); i++) {
+            char op = exp.charAt(i);
+
+            if (checkIsItNumber(op)) {
+                int number = Integer.parseInt(String.valueOf(exp.charAt(i)));
+                postfix.append(number);
+                postfix.append(" "); //если число - добавляем его в стрингбилдер
+            }
+//если нам попался оператор
+            else if (op == '+' || exp.charAt(i) == '-' || op == '/' || op == '*') {
+                if (operators.isEmpty() || operators.peek() == '(') {
+                    operators.push(op);
+                } //Если стек (STACK) пуст или содержит левую скобку в вершине (TOP), то добавляем (PUSH) входящий оператор в стек (STACK).
+                else if (getPriority(op) > getPriority(operators.peek())) { //getPriority + = 2, getPriority * = 3
+                    operators.push(op);
+                }//Если входящий оператор имеет более высокий приоритет чем вершина (TOP), поместите (PUSH) его в стек (STACK).
+                else if (getPriority(op) < getPriority(operators.peek()) || getPriority(op) == getPriority(operators.peek())) {
+                    while (!(operators.isEmpty()) && (getPriority(op)<getPriority(operators.peek()) || getPriority(op)==getPriority(operators.peek()))){
+                        postfix.append(operators.pop());
+                        postfix.append(" "); }
+                    operators.push(op);
+                }
+//Если входящий оператор имеет более низкий или равный приоритет, чем вершине (TOP), выгружаем POP в строку,
+// пока не увидите оператор с меньшим приоритетом или левую скобку на вершине (TOP), затем добавьте (PUSH) входящий оператор в стек (STACK).
+            } else if (op == '(') {
+                operators.push(op);
+            } else if (op == ')') {
+                while (operators.peek() != '(') {
+                    postfix.append(operators.pop());
+                    postfix.append(" ");
+                }
+                operators.pop();
+            }//Если входящий элемент является правой скобкой,
+// выгружаем стек (POP) и добавляем его элементы в очередь (QUEUE), пока не увидите левую круглую скобку. Удалите найденную скобку из стека (STACK).
+//else postfix.append(operators.pop());
+
+        }
+        while (operators.size()!=0) {
+            postfix.append(operators.pop());
+            postfix.append(" ");
+        }
+        return postfix.toString();
+    }
+    public static int getPriority(char operator) {
+        int priority = 0;
+        if (operator == '(') priority = 4;
+        if (operator == ')') priority = 4;//4 cамый высокий
+        if (operator == '+') priority = 2;
+        if (operator == '-') priority = 2; // низкий приоритет
+        if (operator == '*') priority = 3;// 3 высокий
+        if (operator == '/') priority = 3;
+        return priority;
+    }
+
+    public static boolean checkIsItNumber(char num) {
+        try {
+            Integer.parseInt(String.valueOf(num));
+            return true;
+        } catch (Exception ex) {
+            return false;
         }
     }
 
